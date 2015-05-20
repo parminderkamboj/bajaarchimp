e1ba5eed5c9ee2f6e723a68cdab8932cc079be86
parminderkamboj <parminder.kamboj@gmail.com>
2015-05-19 23:16:53 -0700

    first commit for project

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var favicon = require('serve-favicon');
+
+
+var app = require('./routecontroller');
+var config = require('./config');
+var db = require('./database');
+
+// view engine setup
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+db.init(config.databaseConfig, false);
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
